cce61e8877fbf15d030f5a77056d13a21c1ae698
GVND SAI PRASAD <40757983+saiprasad1586@users.noreply.github.com>
2021-09-05 14:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run( port=port)
-    # host='0.0.0.0
+    app.run(host='0.0.0.0', port=port)
+    # 
