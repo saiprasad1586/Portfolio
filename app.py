bf059551665f00e10d812b984f4787411b8b8f13
GVND SAI PRASAD <40757983+saiprasad1586@users.noreply.github.com>
2022-11-04 16:07:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask,redirect,url_for,render_template,request
 import flask
 from flask_cors import CORS,cross_origin
-import pymongo as pmb
+# import pymongo as pmb
 import datetime
 import os
 app=Flask(__name__)
 
-client = pmb.MongoClient('mongodb+srv://Saiprasad:Srihari@cluster0.kkyyt.mongodb.net/WebIp?retryWrites=true&w=majority')
+# client = pmb.MongoClient('mongodb+srv://Saiprasad:Srihari@cluster0.kkyyt.mongodb.net/WebIp?retryWrites=true&w=majority')
 
-db = client['mywebip']
+# db = client['mywebip']
 
 
 @app.route('/',methods=['GET','POST'])
 @cross_origin()
 def home():
-    ip_address = str(request.remote_addr)
-    print(ip_address)
-    db.ipaddr.insert_one({"timeStamp": datetime.datetime.now(),"ip_Address":ip_address})
+#     ip_address = str(request.remote_addr)
+#     print(ip_address)
+#     db.ipaddr.insert_one({"timeStamp": datetime.datetime.now(),"ip_Address":ip_address})
     return render_template('index.html')
 
 @app.route('/static/Resume.pdf',methods=['GET','POST'])
